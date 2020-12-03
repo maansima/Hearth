f5a9649f030d8cb806b8f6923f5fdb60cc0ed7d7
Maansi Manchanda <maansima@usc.edu>
2020-12-03 13:37:19 -0800

    added animation

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -12,32 +12,60 @@ class MainPage extends React.Component {
     let xPos = window.innerWidth/2;
     let yPos = window.innerWidth/8;
     let beads = [];
+    
 
     p.setup = () => {
         p.createCanvas(window.innerWidth, window.innerHeight);
+        p.fill(232, 230, 223);
+        var circles = 16;
+        var angle = Math.PI*2 / circles;
+        var radius = 230;
+        for(var i = 0; i < circles; i++){
+            var xCircle = xPos + p.cos(angle*i) * radius;
+            var yCircle = yPos - p.sin(angle*i) * radius;
+            p.noStroke();
+            beads.push(new Bead(xCircle, yCircle, 80)); 
+        }
     };
 
     p.draw = () => {
-        p.fill(229,83,0);
-        var middle = window.innerWidth/8;
-        var circles = 16;
-        var angle = Math.PI*2 / circles;
-        var radius = 200;
-        
-    
-        for(var i = 0; i < circles; i++){
-    
-            var xCircle = xPos + p.cos(angle*i) * radius;
-            var yCircle = yPos - p.sin(angle*i) * radius;
-    
-            p.ellipse(xCircle, yCircle, 60);
+        let s = p.second();
+        if((s%16 >= 0 && s%16 < 4) || (s%16 >= 8 && s%16 < 12)){
+            beads[s%16].fill(245, 158, 66);
+        } else {
+            beads[s%16].fill(255, 202, 10);
         }
-        p.noStroke();
+       
+        if(s%16 === 0) {
+            for(var i = 0; i < beads.length; i++){
+                beads[i].fill(232, 230, 223);
+            }
+        }
     };
 
     p.windowResized = () => {
         p.resizeCanvas(window.innerWidth, window.innerHeight);
       };
+
+    class Bead {
+
+        constructor(x, y, diameter) {
+            this.x = x;
+            this.y = y; 
+            this.diameter = diameter; 
+            this.display();
+          }
+        
+          display() {
+            p.ellipse(this.x, this.y, this.diameter);
+            p.noStroke();
+          }
+
+          fill(r, g, b) {
+              this.display();
+              p.fill(r, g, b);
+          }
+    }
 
   };
 
